9c789f12f6d0c804a9665838dd685b07bb9a705c
glee <gleehave@gmail.com>
2021-10-10 14:22:36 +0900

    왜.. ? 안되지?

--- Stack/괄호.py
+++ Stack/괄호.py
@@ -1,6 +1,31 @@
-test = input()
-print(test)
-#T = int(input())
+T = int(input())
+res=[]
+Data=[]
 
-#for _ in range(T):
-#    Question = input()
+for _ in range(T):
+    Question = input()
+    Data.append(Question)
+
+
+for i in Data:
+    count = 0
+    res_count = True
+    for j in i:
+        if count == 0:
+            if j == ')':
+                res.append('NO')         
+                res_count = False   
+                break
+        if j == '(':
+            count += 1
+       
+        if j == ')':
+            count -= 1
+    if res_count  != False:
+        if count == 0:
+            res.append('YES')
+        else:
+            res.append('NO')
+
+
+print(res)
