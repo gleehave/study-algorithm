8737e436ff4569e58f2d4303b6bafb5912246cb8
glee <gleehave@gmail.com>
2021-10-10 14:28:39 +0900

    count의 값이 -1이 되는 경우로 범위를 잡는다. : if count < 0:

--- Stack/괄호.py
+++ Stack/괄호.py
@@ -1,31 +1,20 @@
 T = int(input())
-res=[]
-Data=[]
 
 for _ in range(T):
-    Question = input()
-    Data.append(Question)
-
-
-for i in Data:
+    Data = input()
+    list_data = list(Data)
     count = 0
-    res_count = True
-    for j in i:
-        if count == 0:
-            if j == ')':
-                res.append('NO')         
-                res_count = False   
-                break
-        if j == '(':
+
+    for i in list_data:
+        if i == '(':
             count += 1
-       
-        if j == ')':
+        elif i == ')':
             count -= 1
-    if res_count  != False:
-        if count == 0:
-            res.append('YES')
-        else:
-            res.append('NO')
-
-
-print(res)
+        
+        if count < 0:
+            print('NO')
+            break
+    if count > 0 :
+        print('NO')
+    elif count == 0:
+        print('YES')
