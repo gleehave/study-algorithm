a32d3ea5abe0b9d2202d7675bae4d5788b8c34e7
leegangho <gleehave@gmail.com>
2021-10-11 21:13:44 +0900

    중간 확인을 위한 출력부분 제거

--- Queue/요세푸스문제.py
+++ Queue/요세푸스문제.py
@@ -3,11 +3,8 @@ N, K = map(int, input().split())
 
 res = []
 Circle = [_ for _ in range(1,N+1)]
-print('Circle: ',Circle)
-
 
 for _ in range(N):
-    print('res: ',res)
     for i in range(K):  # i = 0, 1, 2
 
         if i+1 == K:
@@ -17,11 +14,14 @@ for _ in range(N):
             break
 
         temp = Circle[0]
-        print("temp: ", temp)
         Circle.append(temp)
         Circle.pop(0)
 
-print("<")
-for _ in res:
-    print(_,sep=',')
+print("<",end='')
+for i in range(N):
+    if i == N-1:
+        print(res[i], end='')
+        break
+    print(res[i], end='')
+    print(', ', end='')
 print(">")
