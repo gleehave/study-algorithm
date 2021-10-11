3a8fc3692aaac28d3ac34e6945ef7f7419f63f91
leegangho <gleehave@gmail.com>
2021-10-11 20:59:27 +0900

    Queue 문제

--- Queue/요세푸스문제.py
+++ Queue/요세푸스문제.py
@@ -1,3 +1,27 @@
+# K는 index다.
 N, K = map(int, input().split())
 
+res = []
 Circle = [_ for _ in range(1,N+1)]
+print('Circle: ',Circle)
+
+
+for _ in range(N):
+    print('res: ',res)
+    for i in range(K):  # i = 0, 1, 2
+
+        if i+1 == K:
+            temp = Circle[0]
+            res.append(temp)
+            Circle.pop(0)
+            break
+
+        temp = Circle[0]
+        print("temp: ", temp)
+        Circle.append(temp)
+        Circle.pop(0)
+
+print("<")
+for _ in res:
+    print(_,sep=',')
+print(">")
