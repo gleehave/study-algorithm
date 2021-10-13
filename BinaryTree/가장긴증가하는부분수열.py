26892eedbb534bb3de330c870eabcf2ec2b8ff56
glee <gleehave@gmail.com>
2021-10-13 16:53:38 +0900

    이분탐색으로 하면 시간복잡도를 줄일 수 있다. O(n^2) -> O(NlogN)

---
+++ BinaryTree/가장긴증가하는부분수열.py
@@ -0,0 +1,20 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+memory = [0]
+
+for case in A:
+    if memory[-1] < case:
+        memory.append(case)
+    else:
+        left = 0
+        right = len(memory)
+        while left < right:
+            mid = (left + right) // 2
+            if memory[mid] < case:
+                left = mid + 1
+            else:
+                right = mid
+        memory[right] = case
+
+print(len(memory)-1)
