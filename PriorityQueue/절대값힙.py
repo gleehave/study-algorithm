d1e15f0026fbd0831efb8e604bff7f8c0b4a4211
leegangho <gleehave@gmail.com>
2021-10-17 22:03:50 +0900

    절대값 힙..

--- PriorityQueue/절대값힙.py
+++ PriorityQueue/절대값힙.py
@@ -1,39 +1,15 @@
-# x != 0 이면, 배열에 x를 추가한다.
-# x = 0 이면, 배열에서 절대값이 가장 각은 값을 출력하고, 배열에서 제거한다.
+import sys
+import heapq
 
-N = int(input())
-arr = []
-queue_plus = []
-queue_minus = []
+n = int(input())
+q = []
 
-for _ in range(N):
-    data = int(input())
-    arr.append(data)
-
-for i in range(N):
-    if arr[i] != 0:
-        if arr[i] > 0:
-            queue_plus.append(arr[i])
-        elif arr[i] < 0:
-            queue_minus.append(arr[i])
-    elif arr[i] == 0:
-
-        if not queue_minus and not queue_plus:
+for i in range(n):
+    a = int(sys.stdin.readline().rstrip())
+    if a != 0:
+        heapq.heappush(q, (abs(a), a))
+    else:
+        if not q:
             print(0)
-            continue
-        queue_minus.sort(reverse=True)
-        queue_plus.sort()
-        print('minus:', queue_minus)
-        print('plus: ', queue_plus)
-        if not queue_minus:
-            print(queue_plus.pop(0))
-            continue
-        if not queue_plus:
-            print(queue_minus.pop(0))
-            continue
-        if min(queue_plus) == abs(max(queue_minus)):
-            print(queue_minus.pop(0))
-            continue
-        elif min(queue_plus) != abs(max(queue_minus)):
-            print(queue_plus.pop(0))      
-            continue
+        else:
+            print(heapq.heappop(q)[1])
