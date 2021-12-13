b17da6f0f6cdd1e89afebb840d5244fce791dc4a
gleehave <gleehave@gmail.com>
2021-12-13 15:38:15 +0900

    bfs 방식..

---
+++ BinaryTree/트리의부모찾기.py
@@ -0,0 +1,26 @@
+# 노드의 개수 n개
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n=int(input())
+graph = [[] for _ in range(n+1)]
+parent = [0 for _ in range(n+1)]
+
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    graph[a].append(b) #
+    graph[b].append(a) # node 인덱스에 맞춘다.
+
+def bfs():
+    q = deque()
+    q.append(1)
+    while q:
+        node = q.popleft()
+        for i in graph[node]:
+            if parent[i] == 0:
+                parent[i] = node
+                q.append(i)
+    return parent
+
