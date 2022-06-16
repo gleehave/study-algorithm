f9fd94d4a8d8dc320a7d7784d8046eec23b9214e
gleehave <gleehave@gmail.com>
2022-06-16 18:34:08 +0900

    [add] 수열합치기

--- SWEA/LinkedList/수열합치기.py
+++ SWEA/LinkedList/수열합치기.py
@@ -39,3 +39,67 @@ for test_case in range(1, T + 1):
     #     result += data[i][:index+1] + data[i+1][:]
     #
     # print("result: ",result)
+
+# class Node:
+#     def __init__(self, d=0, p=None, n=None):
+#         self.data = d
+#         self.prev = p
+#         self.next = n
+#
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+#         self.tail = None
+#         self.size = 0
+#
+# def addList(lst, arr):
+#     first = last = Node(arr[0])
+#     for val in arr[1:]:
+#         new = Node(val, last)
+#         last.next = new
+#         last = new
+#
+#     if lst.head is None:
+#         lst.head, lst.tail = first, last
+#     else:
+#         cur = lst.head
+#         while cur is not None:
+#             if cur.data > arr[0]:
+#                 break
+#             cur = cur.next
+#
+#         if cur is None:
+#             first.prev = lst.tail
+#             lst.tail.next = first
+#             lst.tail = last
+#         elif cur.prev is None:
+#             last.next = lst.head
+#             lst.head.prev = last
+#             lst.head = first
+#         else:
+#             prev = cur.prev
+#             first.prev = prev
+#             last.next = cur
+#             prev.next = first
+#             cur.prev = last
+#     lst.size += len(arr)
+#
+# def printList(lst):
+#     if lst.head is None:
+#         return
+#     cur = lst.tail
+#     cnt = 10
+#     while cnt:
+#         print(cur.data, end='')
+#         cur=cur.prev
+#         cnt -= 1
+#     print()
+#
+# for tc in range(1, int(input())+1):
+#     n,m = map(int, input().split())
+#     arr = list(list(map(int, input().split())) for _ in range(m))
+#     mylist = LinkedList()
+#     for i in range(M):
+#         addList(mylist, arr[i])
+#     print(f"#{tc}", end='')
+#     printList(mylist)
