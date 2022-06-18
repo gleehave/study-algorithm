2bfa07523185a38005a30cde9c61be39583cbf0e
gleehave <gleehave@gmail.com>
2022-06-18 11:47:16 +0900

    [modify] description

--- SWEA/advanced/implementation/이진수2.py
+++ SWEA/advanced/implementation/이진수2.py
@@ -2,27 +2,10 @@
 # N을 소수점 아래 12자리 이내인 이진수로 표시할 수 있으면 0.을 제외한 나머지 숫자를 출력하고,
 # 13 자리 이상이 필요한 경우에는 'overflow'를 출력한다.
 
-# T = int(input())
-# for test_case in range(1, T + 1):
-#     N = float(input())
-#     result = ''
-#     i=1
-#
-#     while True:
-#         if i >= 13:
-#             print("#{} overflow".format(test_case))
-#             break
-#         N *= 2
-#         if N == 1:
-#             result += '1'
-#             print("#{} {}".format(test_case, result))
-#             break
-#         elif N > 1:
-#             N -= 1
-#             result += '1'
-#         else:
-#             result += '0'
-#         i+=1
+# -1 ~ -13 까지 반복
+# N을 2^power로 나눈 몫을 result에 추가
+# 나머지를 N으로 갱신
+# N == 0이면 변환 완료
 
 for tc in range(1, int(input())+1):
     N = float(input())
