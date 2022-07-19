7f25cee3df81ef6f357e5de72c9e00945da67f43
gleehave <gleehave@gmail.com>
2022-07-19 23:26:00 +0900

    [add] 두 정수 사이의 합

--- 프로그래머스/Level1/두정수 사이의 합.py
+++ 프로그래머스/Level1/두정수 사이의 합.py
@@ -1,2 +1,12 @@
-def adder(a, b):
-    return sum(range(min(a,b), max(a,b) + 1))
+def solution(a, b):
+    answer = 0
+    if a == b:
+        return a
+    elif a > b:
+        for i in range(b, a+1):
+            answer += i
+        return answer
+    else:
+        for i in range(a, b+1):
+            answer += i
+        return answer
